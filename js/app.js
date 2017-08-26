4f20944538f929947fd28806b1d744204daa11c8
pia smith <basiccreation@yahoo.com>
2017-08-26 16:57:28 -0700

    added map

--- js/app.js
+++ js/app.js
@@ -4,18 +4,31 @@
 //MAP
 
 // Create variable to hold map
-var map;
+//var map;
 
 // Create a new blank array for all the listing markers.
-var markers = [];
+//var markers = [];
 
 //Create new map
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: 40.7413549, lng: -123.9980244 },
-        zoom: 8
-    })
-    };
+    function initMap() {
+        var initialPosition = {lat: 37.7413549, lng: -121.9980244 };
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: initialPosition,
+            zoom: 9
+        });
+        }
+
+      // function initMap() {
+      //   var uluru = {lat: -25.363, lng: 131.044};
+      //   var map = new google.maps.Map(document.getElementById('map'), {
+      //     zoom: 4,
+      //     center: uluru
+      //   });
+      //   var marker = new google.maps.Marker({
+      //     position: uluru,
+      //     map: map
+      //   });
+      // }
 // $.get(
 //    url,
 //    function(response) {
