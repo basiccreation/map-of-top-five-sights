01689acb09a631f054d56c46906176f14e8a3b55
pia smith <basiccreation@yahoo.com>
2017-08-26 16:24:12 -0700

    added favicon

--- js/app.js
+++ js/app.js
@@ -1,141 +1,21 @@
 //Neighborhood Map
-
 "use strict";
-
-
-// Buildings
-
-
-
-function Building(name, address, website, imgSrc, wikiLink, description, location) {
-    var that = this;
-    that.name = ko.observable(name);
-    that.address = ko.observable(address);
-    that.website = ko.observable(website);
-    that.imgSrc = ko.observable(imgSrc);
-    that.wikiLink = ko.observable(wikiLink);
-    that.description = ko.observableArray(description);
-    that.location = ko.observable(location);
-};
-
-var viewModel = {
-    buildings: ko.observableArray([]),
-    filter: ko.observable(""),
-    search: ko.observable(""),
-};
-
-//ko.utils.arrayFilter - filter the buildings using the filter text
-viewModel.filteredItems = ko.dependentObservable(function() {
-
-    var filter = this.filter().toLowerCase();
-
-    var stringStartsWith = function(string, startsWith) {
-        string = string || "";
-        if (startsWith.length > string.length)
-            return false;
-        return string.substring(0, startsWith.length) === startsWith;
-    };
-
-    if (!filter) {
-        return this.buildings();
-    } else {
-        return ko.utils.arrayFilter(this.buildings(), function(item) {
-            return ko.utils.stringStartsWith(item.name().toLowerCase(), filter);
-        });
-    }
-}, viewModel);
-
-// //a JSON string of buildings
- var list_Of_Buildings = '[{"name":"Peach House","address":"34 Fruits Lane","website":"233.com","imgSrc":"photo.jpg","wikiLink":"wiki.org","description":["heres a desc"]},{"name":"Plum Castle","address":"45 Fruits Lane","website":"75.com","imgSrc":"photo.jpg","wikiLink":"wiki.org","description":["heres a desc"]},{"name":"Donut cottage","address":" 234 Bread Alley","website":"15.us","imgSrc":"photo.jpg","wikiLink":"wiki.org","description":["heres a desc"]},{"name":"Milky Way","address":"90 Dairy Drive","website":"450.com","imgSrc":"photo.jpg","wikiLink":"wiki.org","description":["heres a desc"]},{"name":"Mars Mansion","address":"890 Fairy Extension","website":"33450.com","imgSrc":"photo.jpg","wikiLink":"wiki.org","description":["heres a desc"]}]';
-
-//parse into an object
-var buildingsJSONtoObject = ko.utils.parseJson(list_Of_Buildings);
-
-//do some basic mapping (without mapping plugin)
-var mappedData = ko.utils.arrayMap(buildingsJSONtoObject, function(item) {
-    return new Building(item.name, item.address, item.website, item.imgSrc, item.wikiLink, item.description, item.location);
-});
-
-
-viewModel.buildings(mappedData);
-
-ko.applyBindings(viewModel, document.getElementById('listOfBuildings'));
 
 //MAP
 
-// // Create variable to hold map
-// var map;
+// Create variable to hold map
+var map;
 
-// // Create a new blank array for all the listing markers.
-// var markers = [];
+// Create a new blank array for all the listing markers.
+var markers = [];
 
-// //Create new map
-// function initMap() {
-//     map = new google.maps.Map(document.getElementById('map'), {
-//         center: { lat: 40.7413549, lng: -123.9980244 },
-//         zoom: 8
-//     });
-//     //create info window
-//     var largeInfowindow = new google.maps.InfoWindow();
-
-//     // Create boundaries variable around all markers
-//     var bounds = new google.maps.LatLngBounds();
-
-//     // Create an array of markers on initialize.
-//     for (var i = 0; i < initialBuildings.length; i++) {
-//         // Get the position from the location array.
-//         var position = initialBuildings[i].location;
-//         var name = initialBuildings[i].name;
-//         var snippet = initialBuildings[i].address;
-
-//         // Create a marker per location, and put into markers array.
-//         var marker = new google.maps.Marker({
-//             map: map,
-//             position: position,
-//             name: name,
-//             snippet: snippet,
-//             animation: google.maps.Animation.DROP,
-//             id: i
-
-//         });
-
-//             // Push the marker to our array of markers.
-//             markers.push(marker);
-
-//         // // Create an onclick event to open an infowindow at each marker.
-//         marker.addListener('mouseover', function() {
-//             populateInfoWindow(this, largeInfowindow);
-//             //Change the marker color
-//         });
-//         bounds.extend(markers[i].position);
-//     }
-//     // Extend the boundaries of the map for each marker
-//     map.fitBounds(bounds);
-// }
-
-// // This function populates the infowindow when the marker is clicked. We'll only allow
-// // one infowindow which will open at the marker that is clicked, and populate based
-// // on that markers position.
-// function populateInfoWindow(marker, infowindow) {
-
-//     // Check to make sure the infowindow is not already opened on this marker.
-//     if (infowindow.marker != marker) {
-//         infowindow.marker = marker;
-//         infowindow.setContent('<h2 >' + marker.name + '</h2>');
-//         infowindow.open(map, marker);
-
-//         // Clears marker property when infowindow is closed.
-//         infowindow.addListener('mouseout', function() {
-//             infowindow.setMarker = null;
-//             marker.setIcon('https://www.google.com/mapfiles/marker.png');
-//         });
-//     }
-// }
-
-//var url = "http://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=Julia%20Morgan";
-//var url = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Julia%20Morgan";
-//var url = "http://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&titles=Main%20Page";
-//
+//Create new map
+function initMap() {
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: { lat: 40.7413549, lng: -123.9980244 },
+        zoom: 8
+    })
+    };
 // $.get(
 //    url,
 //    function(response) {
