468387aa259f191e7d843e61967602bcaa6cb627
pia smith <basiccreation@yahoo.com>
2017-09-02 18:07:00 -0700

    added variables

--- js/app.js
+++ js/app.js
@@ -1,44 +1,53 @@
 //Neighborhood Map
 "use strict";
 
-
-//   //$("#articlesHeader").fadeIn();
-//   var searchTerm = document.getElementById("zipcode").value;
-// // google key AIzaSyDVXItUkHHslRJty9xzj4bxMtjTHzGQyc0
-//   var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&type=restaurant&keyword=cruise&key=AIzaSyB3bLgW65HcS0cTzwUYm6S9DdyhaE0xze0";// + searchTerm;
-// $("searchButton").click(function(){
-//     $.getJSON(url, function(result){
-//         $("#details").html(JSON.stringify(result, null, 4));
-//         });
-//     });
-//-------------------------------------
+// google key AIzaSyDVXItUkHHslRJty9xzj4bxMtjTHzGQyc0
 
 //Create new map
 
+var coffeeShop = function(data, self) {
+    this.name = ko.observable(data)
+}
+
 var map;
+var markers;
+var service;
+var infowindow = new google.maps.InfoWindow({size: new google.maps.Size(150,50)});
 
 function initMap() {
-  var initialPosition = {lat: 37.7413549, lng: -122.25};
 
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: initialPosition,
-    zoom: 17
-  });
+    var initialPosition = {lat: 37.8513549, lng: -122.25};
 
-          var marker = new google.maps.Marker({
-          position: initialPosition,
-          map: map
-        });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: initialPosition,
+        zoom: 17
+    });
 
-  var service = new google.maps.places.PlacesService(map);
-  service.nearbySearch({
-    location: initialPosition,
-    radius: 5000,
-    type: ['cafe'],
-    keyword: 'cafe',
-    openNow: true
-  }, processResults);
-}
+    var service = new google.maps.places.PlacesService(map);
+
+    var marker = new google.maps.Marker({
+        position: initialPosition,
+        map: map
+    });
+
+    infowindow = new google.maps.InfoWindow();
+
+
+
+    service.textSearch({
+        location: initialPosition,
+        radius: 1000,
+        type: ['cafe'],
+        keyword: 'cafe',
+        openNow: true
+        }, processResults);
+
+    function callback(place, status) {
+        if (status === google.maps.places.PlacesServiceStatus.OK) {
+        createMarker(place);
+        }
+    }
+} // end initMap
 
 function processResults(results, status, pagination) {
   if (status !== google.maps.places.PlacesServiceStatus.OK) {
@@ -58,7 +67,7 @@ function processResults(results, status, pagination) {
     }
     //TODO else hide more button
   }
-}
+} // end processResults
 
 function createMarkers(places) {
   var bounds = new google.maps.LatLngBounds();
@@ -76,13 +85,29 @@ function createMarkers(places) {
     var marker = new google.maps.Marker({
       map: map,
       icon: image,
-      title: place.name,
-      position: place.geometry.location
+      position: place.geometry.location,
+      animation: google.maps.Animation.DROP,
     });
+
+
+    // google.maps.event.addListener(marker, 'click', function() {
+    //   infowindow.setContent(place.name);
+    //   infowindow.open(map, this);
+    //   });
+
+    // Removes the markers from the map, but keeps them in the array.
+    function clearMarkers() {
+      setMapOnAll(null);
+    }
+
+    // Shows any markers currently in the array.
+    function showMarkers() {
+      setMapOnAll(map);
+    }
 
     placesList.innerHTML += '<li>' + place.name + '</li>';
 
     bounds.extend(place.geometry.location);
   }
   map.fitBounds(bounds);
-}
+} // end createMarkers
