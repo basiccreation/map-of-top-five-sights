932fd2556b987d648c6ded7f1a9da19e51dfb729
pia smith <basiccreation@yahoo.com>
2017-09-09 15:42:29 -0700

    added array of signts

--- js/app.js
+++ js/app.js
@@ -1,2 +1,42 @@
 //Neighborhood Map
 "use strict";
+
+
+//Array of initial 5 locations that are set by default
+var initialLocations = [{
+
+    title: 'Stændertorvet', //initial location
+    location: {
+        lat: 55.64140864743973,
+        lng: 12.081061005592346
+    }
+}, {title: 'Roskilde Cathedral',
+    location: {
+        lat: 55.3834,
+        lng: 12.448
+    }
+}, {
+    title: 'Roskilde Festival',
+    location: {
+        lat: 55.3718,
+        lng: 12.436
+    }
+}, {
+    title: 'Viking Ship Museum',
+    location: {
+        lat: 55.3905,
+        lng: 12.4442
+    }
+}, {
+    title: 'Risø',
+    location: {
+        lat: 55.693,
+        lng: 12.100
+    }
+}, {
+    title: 'Land of Legends -- Lejre',
+    location: {
+        lat: 55.3657,
+        lng: 11.5638
+    }
+}];
